5f3990b30066d81622091658e8a56e95383bcddf
Kai Liu <einbert-xeride@outlook.com>
2020-11-10 02:04:46 +0800

    Upgraded to openttd-jgrpp 0.39.0

--- build/install.sh
+++ build/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -ex
-jgrpp_version=0.38.1
+jgrpp_version=0.39.0
 opengfx_version=0.6.0
 cd /build/
 apt-get update
@@ -22,7 +22,7 @@ apt-get install -y \
     libpng-dev
 wget https://github.com/JGRennison/OpenTTD-patches/archive/jgrpp-$jgrpp_version.tar.gz
 wget https://cdn.openttd.org/opengfx-releases/$opengfx_version/opengfx-$opengfx_version-all.zip
-echo "126b9eca881d33982e078d3e2d55a852fc8c43b95dfcb3817f312cbeb10d16c6 *jgrpp-$jgrpp_version.tar.gz" | sha256sum -c
+echo "2bbbea48bb1c2fb7eae07eb98b4d49654b4b022b491f83ec1df9b6f0803bef54 *jgrpp-$jgrpp_version.tar.gz" | sha256sum -c
 echo "d419c0f5f22131de15f66ebefde464df3b34eb10e0645fe218c59cbc26c20774 *opengfx-$opengfx_version-all.zip" | sha256sum -c
 tar -xvf jgrpp-$jgrpp_version.tar.gz
 cd OpenTTD-patches-jgrpp-$jgrpp_version/bin
